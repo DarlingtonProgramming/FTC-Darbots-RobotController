918c86430f560fc15becad0201b40ce1056c02cf
Andrew <1731095638@qq.com>
2022-07-16 15:26:17 +0800

    Update SumoBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy_2021/Andrew/SumoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy_2021/Andrew/SumoBot.java
@@ -119,6 +119,7 @@ public class SumoBot extends LinearOpMode {
                 // Show the elapsed game time and wheel power.
                 telemetry.addData("Status", "Run Time: " + runtime.toString());
                 telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+                telemetry.addData("Speed:", speed);
                 telemetry.update();
             }
         }
